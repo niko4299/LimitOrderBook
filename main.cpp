1f1880630ce4d966c39c5a41b16c5f4da64f27bc
niko4299 <niko.krvavica@gmail.com>
2023-09-07 22:50:11 +0200

    feat: add ringbuffer implementation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
+#include<iostream>
+#include "utils/ringbuffer.hpp"
 
 int main(int argc, char* argv[]) {
+  auto rb = RingBuffer<int>(10);
+  int i = 1;
+  rb.push(i);
+  int j;
+  rb.pop(j);
+
+  std::cout<<i<<" "<<j;
   return 0;
 }
---
+++ utils/ringbuffer.hpp
@@ -0,0 +1,63 @@
+#include<atomic>
+#include<memory>
+#include<vector>
+
+template<typename T>
+class RingBuffer {
+
+    public:
+
+        RingBuffer(std::size_t capacity): _capacity{capacity}, _data(capacity) {}
+
+        ~RingBuffer() = default;
+        
+        void push(T value){
+            std::uint64_t write_pos;
+            std::uint64_t next_write_pos;
+            while(true){
+                write_pos = _write_pos.load(std::memory_order_relaxed);
+                next_write_pos = write_pos + 1;
+                if (next_write_pos == _read_pos_cache) {
+                    _read_pos_cache = _read_pos.load(std::memory_order_acquire);
+                    if (next_write_pos != _read_pos_cache) {
+                        break;
+                    }
+                } else{
+                    break;
+                }
+            }
+
+            next_write_pos%=_capacity;
+            _data[write_pos] = T(std::forward<T>(value));
+
+            _write_pos.store(next_write_pos, std::memory_order_release);
+
+            return;
+            
+        }
+
+        bool pop(T& value){
+            auto read_pos = _read_pos.load(std::memory_order_relaxed);
+
+            if (read_pos == _write_pos_cache) {
+                _write_pos_cache = _write_pos.load(std::memory_order_acquire);
+                if (read_pos == _write_pos_cache) {
+                    return false;
+                }
+            }
+
+            value = std::move(_data[read_pos++]);
+            read_pos %= _capacity;
+            _read_pos.store(read_pos, std::memory_order_release);
+            return true;
+        }
+
+
+    private:
+        std::uint64_t _capacity;
+        std::atomic<std::uint64_t> _read_pos{0};
+        std::uint64_t _read_pos_cache{0};
+        std::atomic<std::uint64_t> _write_pos{0};
+        std::uint64_t _write_pos_cache{0};
+        std::vector<T> _data;
+};
